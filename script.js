2908d930af75665476cbc5b83ff08610e38dbfc3
mvillarroeltab <53697657+mvillarroeltab@users.noreply.github.com>
2022-10-21 12:51:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function myFunction(id) {
   element.classList.add("description-display"+id);
   console.log ("hello");
 }
+function myFunction2(id) {
+  var element = document.getElementById("description-p"+id);
+  element.classList.add("description-p"+id);
+  console.log ("hello");
+}
