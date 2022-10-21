fa4a5e76d61b5e9a6db00396d6bfed6e6afda32e
mvillarroeltab <53697657+mvillarroeltab@users.noreply.github.com>
2022-10-21 14:13:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-function myButton(id){
-var Bttn = document.getElementById("button"+id);
-}
 
 function myFunction(id) {
   var element = document.getElementById("description-p"+id);
   element.classList.toggle("description-display"+id);
   console.log ("hello");
 }
-
-button1.addEventListener('click', myFunction(1));
-button2.addEventListener('click', myFunction(2));
-button3.addEventListener('click', myFunction(3));
