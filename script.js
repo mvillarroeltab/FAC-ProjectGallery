3bb67f304667709923e587deef89dc962554f977
mvillarroeltab <53697657+mvillarroeltab@users.noreply.github.com>
2022-10-21 13:23:19 +0100

    Update script.js
    Optional way to remove function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+function myButton(id){
+var Bttn = document.getElementById("button"+id);
+}
+
 function myFunction(id) {
   var element = document.getElementById("description-p"+id);
   element.classList.add("description-display"+id);
   console.log ("hello");
 }
-function myFunction2(id) {
-  var element = document.getElementById("description-p"+id);
-  element.classList.remove("description-display"+id);
-  console.log ("hello");
-}
+
+Bttn.addEventListener('click', myFunction);
+Bttn.removeEventListener('click', myFunction);
